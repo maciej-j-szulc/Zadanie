1a92ec207d68e4db227e4973b1296600eea243bb
maciej-j-szulc <79467814+maciej-j-szulc@users.noreply.github.com>
2023-12-25 22:45:50 +0100

    TaskAppData for backend

---
+++ TaskApp.Data/Fall.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskApp.Data
+{
+    public class Fall
+    {
+        public int Id { get; set; }
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+        public DateTime time_of_fall { get; set; }
+    }
+}
